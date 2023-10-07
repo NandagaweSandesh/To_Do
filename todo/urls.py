9c000c100ee3ef3b3c6df465d14e237cccaa7167
Sandesh Nandagawe <sandagawe@gmail.com>
2023-10-07 16:40:20 +0530

    ToDo added functionality: Add, update, delete,mark-done, undo

--- todo/urls.py
+++ todo/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
 
     #ADD TASK
     path('add_task/', views.addTask, name= 'addTask'),
+    #MARK AS DONE
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name= 'mark_as_done'),
+    #EDIT TASK
+    path('edit_task/<int:pk>/', views.edit_task, name= 'edit_task'),
+    #DELETE TASK
+    path('delete_task/<int:pk>/', views.delete_task, name= 'delete_task'),
+    #UNDO TASK
+    path('undo_task/<int:pk>/', views.undo_task, name= 'undo_task'),
+
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -8,8 +8,10 @@ from todo.models import Task
 def home(request):
     # return HttpResponse("Home Page")
     tasks = Task.objects.filter(is_completed = False).order_by('-updated_at')
+    tasks_completed = Task.objects.filter(is_completed = True).order_by('-updated_at')
     context = {
         'tasks' :tasks,
+        'tasks_completed' : tasks_completed,
     }
 
     return render(request, 'home.html', context)
